d7a166e43e528af993bb2ec234fd1cf2e1587e0e
dengzemiao <dengzemiaovip@163.com>
2021-04-12 11:56:19 +0800

    优化全链接下载

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-04-09 19:17:39
+ * @LastEditTime: 2021-04-12 11:55:55
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /DZMH5Dowload/src/views/Home.vue
+-->
 <template>
   <div>
-    <div class="dowload-btn" @click="touchProxyDowload1">按钮-代理下载</div>
-    <div class="dowload-btn" @click="touchProxyDowload2">按钮-代理下载-自定义文件名</div>
+    <div class="dowload-btn" @click="touchProxyDowload1">按钮-链接下载</div>
+    <div class="dowload-btn" @click="touchProxyDowload2">按钮-代理下载</div>
+    <div class="dowload-btn" @click="touchProxyDowload3">按钮-代理下载-自定义文件名</div>
+    <div class="dowload-btn">
+      <a href="https://file.hepai.video/local/other/6c5d7e97341b2d2806917ee797034835.png" download>a标签-链接下载（跟网页非同域名的只能打开，无法下载，开启访问权限也不行，只能走代理或文件存放自己服务器走同域名下载）</a>
+    </div>
     <div class="dowload-btn">
       <a href="/dowload2/files/pic/pic9/202103/hpic3704.jpg" download>a标签-代理下载</a>
     </div>
@@ -16,24 +28,33 @@ import { DOWLOAD_FILE, DOWLOAD_FILE_PRO } from './dowload'
 export default {
   data () {
     return {
-      // 下载测试连接，百度随便搜的图片地址:
-
+      // 下载测试连接，百度随便搜的图片地址，未开启访问权限地址:
       // https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201311%2F17%2F174124tp3sa6vvckc25oc8.jpg&refer=http%3A%2F%2Fattach.bbs.miui.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1618976641&t=ca698f4ed0e96eb2f269ccff644bda03
-
       // https://scpic.chinaz.net/files/pic/pic9/202103/hpic3704.jpg
-
       // https://file.hepai.video/local/other/3c746d682c7c821c7a792698625dac45.mp4
+
+      // 开启了访问权限地址
+      // https://file.hepai.video/local/other/6c5d7e97341b2d2806917ee797034835.png
     }
   },
   methods: {
-    // 按钮-代理下载（代理配置存放在 vue.config.js，也可以放在服务器中配置）
+    // 按钮-链接下载
     touchProxyDowload1 () {
+      // 下载指定全链接
+      // 由于服务器配置不一样，有的链接会报跨域错误，如果报跨域错误只能走代理下载，也可以要求服务器修改文件访问权限配置
+      // 例如：服务器打开权限运行访问（七牛地址）下载成功
+      DOWLOAD_FILE('https://file.hepai.video/local/other/6c5d7e97341b2d2806917ee797034835.png')
+      // 例如：服务器没有打开权限运行访问（百度图片）下载失败，跨域错误，那就只能走跨域
+      // DOWLOAD_FILE('https://scpic.chinaz.net/files/pic/pic9/202103/hpic3704.jpg')
+    },
+    // 按钮-代理下载（代理配置存放在 vue.config.js，也可以放在服务器中配置）
+    touchProxyDowload2 () {
       // 有后缀的则会使用当前后缀图片名称，也可以像上面那样自定义
       DOWLOAD_FILE('/files/pic/pic9/202103/hpic3704.jpg', '/dowload2')
       // DOWLOAD_FILE('/local/other/3c746d682c7c821c7a792698625dac45.mp4', '/dowload3')
     },
     // 按钮-代理下载-自定义文件名（代理配置存放在 vue.config.js，也可以放在服务器中配置）
-    touchProxyDowload2 () {
+    touchProxyDowload3 () {
       // 这种没有后缀的图片需要自己传入图片名称带后缀.png .jpg ...
       DOWLOAD_FILE_PRO('/image_search/src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201311%2F17%2F174124tp3sa6vvckc25oc8.jpg&refer=http%3A%2F%2Fattach.bbs.miui.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1618976641&t=ca698f4ed0e96eb2f269ccff644bda03', 'avatar.png', '/dowload1')
     }
--- src/views/dowload.js
+++ src/views/dowload.js
@@ -1,15 +1,22 @@
 // 参考文章 https://blog.csdn.net/zz00008888/article/details/113847294
 
-// 下载文件
+/**
+ * @description: 下载文件
+ * @param {*} url 参考 DOWLOAD_FILE_PRO()
+ * @param {*} proxy 参考 DOWLOAD_FILE_PRO()
+ * @param {*} proxyhttp 参考 DOWLOAD_FILE_PRO()
+ */
 export function DOWLOAD_FILE (url, proxy, proxyhttp) {
   DOWLOAD_FILE_PRO(url, '', proxy, proxyhttp)
 }
 
-// 下载文件
-// url: 下载链接。（例如：全链接(http://dowload.file/dzm.png) || 除去代理后剩余的部分链接(/dzm.png))
-// filename: 下载文件名称，为空则取链接尾部文件名。（例如：dzm.png）
-// proxy: 配置好的代理协议。(例如：/dowload， /dowload == http://dowload.file)
-// proxyhttp: 代理协议的链接地址，如果配置则会替换 url 中该段代理链接地址为 proxy 代理协议，所以 proxyhttp 有值，proxy 必须有值。(例如：http://dowload.file)
+/**
+ * @description: 下载文件
+ * @param {*} url 下载链接。（例如：全链接(http://dowload.file/dzm.png) || 除去代理后剩余的部分链接(/dzm.png))
+ * @param {*} filename 下载文件名称，为空则取链接尾部文件名。（例如：dzm.png）
+ * @param {*} proxy 配置好的代理协议。(例如：/dowload， /dowload == http://dowload.file)
+ * @param {*} proxyhttp 代理协议的链接地址，如果配置则会替换 url 中该段代理链接地址为 proxy 代理协议，所以 proxyhttp 有值，proxy 必须有值。(例如：http://dowload.file)
+ */
 export function DOWLOAD_FILE_PRO (url, filename, proxy, proxyhttp) {
   // 文件名称
   var fname = filename
@@ -25,18 +32,56 @@ export function DOWLOAD_FILE_PRO (url, filename, proxy, proxyhttp) {
   if (proxyhttp && proxy) {
     // 替换代理链接地址为代理协议
     dowloadURL = dowloadURL.replace(proxyhttp, proxy)
+    // 代理连接下载
+    DOWLOAD_FILE_URL_PROXY(dowloadURL, fname)
   } else if (proxy) {
     // 将下载链接匹配上代理协议
     dowloadURL = proxy + dowloadURL
+    // 代理连接下载
+    DOWLOAD_FILE_URL_PROXY(dowloadURL, fname)
+  } else {
+    // 连接下载
+    DOWLOAD_FILE_URL(dowloadURL, fname)
   }
+}
+
+/**
+ * @description: 下载指定代理链接
+ * @param {*} url 拼接好的代理连接
+ * @param {*} filename 文件名称
+ */
+export function DOWLOAD_FILE_URL_PROXY (url, filename) {
   // 创建一个a节点插入的document
   var a = document.createElement('a')
   // 模拟鼠标click点击事件
   var event = new MouseEvent('click')
   // 设置a节点的download属性值
-  a.download = fname
+  a.download = filename
   // 将需要下载的URL赋值给a节点的href
-  a.href = dowloadURL
+  a.href = url
   // 触发鼠标点击事件
   a.dispatchEvent(event)
+}
+
+/**
+ * @description: 下载指定链接
+ * @param {*} url 非代理的正常链接
+ * @param {*} filename 文件名称
+ */
+ export function DOWLOAD_FILE_URL (url, filename) {
+  // 获取链接二进制数据
+  fetch(url).then(res => res.blob().then(blob => {
+    // 创建一个a节点
+    var a = document.createElement('a')
+    // 创建一个可供下载链接
+    var url = window.URL.createObjectURL(blob)
+    // 将需要下载的URL赋值给a节点的href
+    a.href = url
+    // 设置节点的download属性值
+    a.download = filename
+    // 触发点击事件
+    a.click()
+    // 释放
+    window.URL.revokeObjectURL(url)
+  }))
 }
